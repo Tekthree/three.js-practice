78f554158c59d6b77f730253d30597ef2d57238b
Tek Jones <tekthree@gmail.com>
2021-10-13 16:14:23 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import * as THREE from "three";
+import React, { useRef, Suspense } from "react";
+import { Canvas, extend, useFrame, useLoader } from "@react-three/fiber";
+import { shaderMaterial } from "@react-three/drei";
+import glsl from "babel-plugin-glsl/macro";
+import image3 from "./crystal.jpg";
 
-function App() {
+import "./App.css";
+
+const WaveShaderMaterial = shaderMaterial(
+  //Unifom
+  {
+    uTime: 0,
+    uColor: new THREE.Color(0.0, 0.0, 0.0),
+    uTexture: new THREE.Texture(),
+  },
+
+  // Vertex Shader
+  glsl`
+  precision mediump float;
+  varying float vWave;
+
+  uniform float uTime;
+
+  #pragma glslify: snoise3 = require(glsl-noise/simplex/3d);
+
+  varying vec2 vUv;
+
+  void main(){
+    vUv = uv;
+
+    vec3 pos = position;
+    float noiseFreq = 1.5;
+    float noiseAmp = 0.25;
+    vec3 noisePos = vec3(pos.x * noiseFreq + uTime, pos.y, pos.z);
+    pos.z += snoise3(noisePos) * noiseAmp;
+    vWave = pos.z;
+
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+    }`,
+  // Fragment Shader
+  glsl`
+    precision mediump float;
+    uniform vec3 uColor;
+    uniform float uTime;
+    uniform sampler2D uTexture;
+    varying vec2 vUv;
+    varying float vWave;
+
+
+    void main(){
+      float wave = vWave * 0.1;
+      vec3 texture = texture2D(uTexture, vUv + wave).rgb;
+      gl_FragColor = vec4(texture, 1.0);
+    }`
+);
+
+extend({ WaveShaderMaterial });
+
+const Wave = () => {
+  const ref = useRef();
+  useFrame(({ clock }) => (ref.current.uTime = clock.getElapsedTime()));
+
+  const [image] = useLoader(THREE.TextureLoader, [
+    image3
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <mesh>
+      <planeBufferGeometry args={[0.4, 0.6, 16, 16]} />
+      <waveShaderMaterial uColor={"pink"} ref={ref} uTexture={image} />
+    </mesh>
   );
-}
+};
+
+const Scene = () => {
+  return (
+    <Canvas camera={{ fov: 9, position: [0, 0, 5] }}>
+      <Suspense fallback={null}>
+        <Wave />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+const App = () => {
+  return <Scene />;
+};
 
 export default App;
